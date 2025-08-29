037d9415e8c408832f18aac803d3247d1457ead9
Daniel Breen <dbreen@kno2.com>
2025-08-28 23:31:57 -0600

    palindrome-number

---
+++ problems/palindrome-number/palindrome-number.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from 'vitest'
+import { isPalindrome } from './palindrome-number.ts'
+
+test.each([
+    [121, true],
+    [-121, false],
+    [11, true],
+    [123, false]
+])("%s returns %s", (num, expected) => {
+    const result = isPalindrome(num);
+    expect(result).toEqual(expected);
+});
