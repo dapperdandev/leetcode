18e6cbff59fba3a930daa6112a40592b5756be21
Daniel Breen <dbreen@kno2.com>
2026-09-10 09:55:47 -0600

    242 - Valid Anagram

--- problems/Arrays & Hashing/242 - Valid Anagram/valid-anagram.ts
+++ problems/Arrays & Hashing/242 - Valid Anagram/valid-anagram.ts
@@ -5,5 +5,14 @@
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 export function isAnagram(s: string, t: string): boolean {
-    throw new Error("Not implemented");
+    if (s.length != t.length) return false;
+    
+    const letters = new Map<string,number>();
+ 
+    for (let i = 0; i < s.length; i++) {
+        letters.set(s[i], (letters.get(s[i]) ?? 0) + 1);
+        letters.set(t[i], (letters.get(t[i]) ?? 0) - 1);
+    }
+
+    return [...letters.values()].every(l => l == 0);
 }
